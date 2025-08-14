200a877a9917c1524b468259a273e261fee17a64
Avery Allison <averyallison02@gmail.com>
2025-08-14 12:57:19 -0700

    Added derivation to MasterKeyDeriver

--- daisypass/src/main/java/com/averyallison/daisypass/crypto/MasterKeyDeriver.java
+++ daisypass/src/main/java/com/averyallison/daisypass/crypto/MasterKeyDeriver.java
@@ -1,10 +1,23 @@
 package com.averyallison.daisypass.crypto;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.PBEKeySpec;
+
 /**
  * Derives a key from a user-entered Master Password
  * <ul>
  *  <li><code>masterPassword</code> - A plaintext master password received at runtime from the user</li>
- *  <li><code>salt</code> - A salt to make the password unique, or null if one has not been generated yet<li>
+ *  <li><code>saltB64</code> - A Base64-encoded salt to make the password unique<li>
+ *  <li><code>DEFAULT_SALT_LENGTH</code> - default length for the public salt in bytes</li>
+ *  <li><code>ITERATION_COUNT</code> - number of times to run password and salt through the algorithm</li>
+ *  <li><code>KEYLENGTH</code> - length of the derived key in bytes</li>
+ *  <li><code>DERIVER_ALGORITHM</code> - algorithm for the key deriver to use</li>
  * </ul>
  * @author Avery Allison <averymallison@proton.me>
  * @version 0.1.0
@@ -13,7 +26,13 @@ package com.averyallison.daisypass.crypto;
 public class MasterKeyDeriver 
 {
     private String masterPassword;
-    private String salt;
+    private String saltB64;
+
+    public static final int DEFAULT_SALT_LENGTH = 16; 
+    public static final int ITERATION_COUNT = 100_000;
+    public static final int KEY_LENGTH = 256;
+
+    private final String DERIVER_ALGORITHM = "PBKDF2WithHmacSHA256";
 
     public String getMasterPassword()
     {
@@ -21,7 +40,7 @@ public class MasterKeyDeriver
     }
 
     /**
-     * Ensures the following about an entered master password:
+     * ensures the following about an entered master password:
      * <ul>
      *  <li>It contains between twelve and twenty characters</li>
      *  <li>It contains at least one number (0-9)</li>
@@ -66,30 +85,51 @@ public class MasterKeyDeriver
         return true;
     }
 
-    public String getSalt()
+    public String getSaltB64()
     {
-        return this.salt;
+        return this.saltB64;
+    }
+
+    public void setSaltB64(String saltB64)
+    {
+        this.saltB64 = saltB64;
     }
 
     /**
-     * Ensures that the entered salt has between 12 and 20 characters.
-     * @param salt The salt to be validated
-     * @return The validity of the salt
+     * generates a Base64 representation of a random salt
+     * @param length the length of the decoded salt, in bytes
+     * @return the encoded salt
      */
-    private boolean validateSalt(String salt)
+    public String generateSalt(int length)
     {
-        if (20 < salt.length() || salt.length() < 12) return false;
-        return true;
+        SecureRandom salter = new SecureRandom();
+        byte[] salt = new byte[length];
+
+        salter.nextBytes(salt);
+
+        return Base64.getEncoder().encodeToString(salt);
     }
 
-    public boolean setSalt(String salt)
+    public String generateSalt()
     {
-        if (!(validateSalt(salt)))
-        {
-            return false;
-        }
+        return generateSalt(DEFAULT_SALT_LENGTH);
+    }
 
-        this.salt = salt;
-        return true;
+    /**
+     * derive a key from the master password and salt fields
+     * @return the resulting key
+     * @throws NoSuchAlgorithmException DERIVER_ALGORITHM is invalid
+     * @throws InvalidKeySpecException keySpec could not be created from password or salt
+     */
+    public SecretKey deriveKey() throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        PBEKeySpec keySpec = new PBEKeySpec(
+            this.masterPassword.toCharArray(), 
+            Base64.getDecoder().decode(saltB64), 
+            ITERATION_COUNT, 
+            KEY_LENGTH);
+        
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DERIVER_ALGORITHM);
+        return keyFactory.generateSecret(keySpec);
     }
 }
